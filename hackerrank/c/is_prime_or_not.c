fdab679adf1a526231040eef7499faa524d94aca
huaqianlee <huaqianlee@gmail.com>
2020-12-16 19:28:50 +0800

    Add time and complexity with c language

---
+++ hackerrank/c/is_prime_or_not.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool is_prime(int n) {
+	for (int i = 2; i * i <= n; ++i) {
+		if (n % i == 0)
+			return false; 
+	}
+
+	return true;
+}	
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+	    int num = 0;
+	    int data[35] = {0};
+
+	    scanf("%d", &num);
+	    for (int i = 0; i < num; ++i) {
+		    while (getchar() != '\n');
+		    scanf("%d", &data[i]);
+	    }
+
+	    for (int i = 0; i < num; ++i) {
+		    if (is_prime(data[i]))
+			    printf("Prime\n");
+		    else 
+			    printf("Not prime\n");
+	    }
+	    
+	    return 0;
+}
+
